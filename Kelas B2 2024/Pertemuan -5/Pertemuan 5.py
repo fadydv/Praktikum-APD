4bfba83f9fae80f6a025a72e41121c516e6b81d9
faady/ᐠﹷ ‸ ﹷ ᐟ\ﾉ <142226344+fadydv@users.noreply.github.com>
2024-10-04 14:29:20 +0700

    Pertemuan 5

---
+++ Kelas B2 2024/Pertemuan -5/Pertemuan 5.py
@@ -0,0 +1,94 @@
+# list = ["a,
+#         2,
+#         True,
+#         [1,2,3]]
+
+# print (list1[-1][2])
+
+
+
+# tas = ["buku", 32, True, 3.14, ["IF", 24]]
+
+# for i in tas:
+#     print (i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+#     print(tas[i])
+
+
+# matkul = ["kalkulus", "fisdas", "pti"]
+# print (matkul)
+
+# matkul.append("Sastra Mesin")
+# matkul.insert(1, "Sastra Mesin")
+
+# matkul[1] = "Fisika Quantum"
+# print(matkul)
+
+# del matkul [1]
+# print(matkul)
+
+# baung = matkul.pop(1)
+# print(matkul)
+
+# print(f"variabel buang ={buang}" )
+
+# prodi = ["Informatika", "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan",
+# "Teknik Pertambangan", "Teknik Elektro",
+# "Teknik Industri", "Teknik Sipil",
+# "Teknik Geologi", "Teknik Kimia"]
+
+
+# print(prodi[::2],"\n", prodi)
+
+# pyrochar = ["hu tao", "klee", "xiangling"]
+# hydrochar = ["furina", "nevuliette", "lapu lapu"]
+
+# party = pyrochar + hydrochar
+# print(pyrochar, "n\")
+# print(hydrochar, "n\")
+# print(party)
+
+# print(pyrochar*3)
+
+# barang = [["sepatu","tas","baju"],
+#           ["pulpen","pensil","laptop"]]
+# # print(barang [1][1])
+# print(barang)
+# for baris in barang:
+#     for kolom in baris:
+#         print(kolom)
+
+nama = ("aldi","daffa","arishyi")
+
+print(nama)
+nama.append("rava")
+print(nama)
+
+
+
+
+
+print(barangtotal)
+
+
+# mahasiswa = (69, "Informatika", "2209106044", "Aldy septian")
+# lalu kita unpacking
+# absen, prodi, = mahasiswa
+# maka ketiga variabel tersebut akan berisi data dari tuple mahasiswa
+# menampilkan variabel
+
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+# print(status)
+
+# mahasiswa = (69, "Informatika", "2209106087", "Aldy septian")
+# print(mahasiswa)
+# mahasiswa = list(mahasiswa)
+# mahasiswa[2] = "2209106076"
+
+# mahasiswa = tuple(mahasiswa)
+# print(mahasiswa)
