338a846112c64580dd7d1efdf686ddef8e69f506
faady/ᐠﹷ ‸ ﹷ ᐟ\ﾉ <142226344+fadydv@users.noreply.github.com>
2024-10-18 13:59:59 +0700

    Pertemuan 7

---
+++ Kelas B2 2024/Pertemuan -7/Pertemuan 7.py
@@ -0,0 +1,82 @@
+# def aku_fungsi ():
+#     print("hello world")
+
+# def tambah(a,b):
+#     hasil = a+b
+
+#     print(hasil)
+
+
+# tambah(10)
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# tambahan = tambah(2,4) + tambah(4,6)
+# # tambahan = 6 + 10
+
+# print(tambahan)
+
+# def pilihan(opsi):
+#     match opsi:
+#         case 1:
+#             print(1)
+#             # return
+#         case 2:
+#             print(2)
+#             # return 
+#     print(3)
+
+# pilihan(2)
+
+# def cetak(nama, nim, **matkul):
+#     print(nama,nim,matkul)
+
+
+
+
+
+# var = 2,4,5
+
+# var1, var2, var3 = var
+
+# print(var)
+# print(var1)
+# print(var2)
+# print(var3)
+
+# import os
+# os.system("cls")
+
+
+#GLOBAL
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "IT Asikk"
+
+# #LOKAL
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+#     var = 1000
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+#     print(kelas)
+
+# info()
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+
+#dalamdef(10)
+
+# def cetak(nama, nim, umur = 18):
+#     print(nama,nim,umur)
+
+
+
+
+if __name__== "__main__":
+    def cetak(nama, nim, umur = 18):
+        print(nama,nim,umur)
+
